aecaed1af2f722f77976ac6a67fa17ad16d28abb
SnegurkaWs <snegurka@web-esse.ru>
2021-12-24 17:48:46 +0500

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<div>
+    <Component {...pageProps} />
+    <footer>
+      <p>2021 Web-esse.ru</p>
+    </footer>
+  </div>
+  )
 }
 
 export default MyApp
