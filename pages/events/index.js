c3a96618b29bb83b3ee2f1e84ed60f36a30156e1
SnegurkaWs <snegurka@web-esse.ru>
2022-01-15 17:52:31 +0500

    ADD: eventsSearch form component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,20 @@
+import { useRouter } from 'next/router';
+
 import EventCard from '../../components/EventCard/EventCard';
+import EventsSearch from '../../components/EventsSearch/EventsSearch';
 import Layout from '../../components/Layout/Layout';
 import { fetchEvents } from '../../lib/events';
 
 export default function HorseEvents(props) {
     const { events } = props;
+
+    const router = useRouter();
+  
+    function findEventsHandler(year, month) {
+      const fullPath = `/events/${year}/${month}`;
+      
+      router.push(fullPath);
+    }
 
     return (
         <div>
@@ -11,6 +22,7 @@ export default function HorseEvents(props) {
             <meta name="description" content="Events" />
 
             <Layout header="Events">
+                <EventsSearch onSearch={findEventsHandler} />
                 {events.length > 0 && (
                     events.map((event) => <EventCard
                         key={event.id}
